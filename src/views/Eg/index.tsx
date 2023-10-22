efa09bb22da01a5f9fe39e248f9a76ae2f893a67
Dilan <dio4@pitt.edu>
2023-10-21 23:14:59 -0500

    tailwind

--- src/views/Eg/index.tsx
+++ src/views/Eg/index.tsx
@@ -3,7 +3,7 @@ import { useSprings, animated, to as interpolate } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 import styles from "./styles.module.css"
 import { TarotCard, cardHeight, cardWidth } from '../../components/TarotCard'
-import { shuffle } from 'lodash-es';
+import { shuffle, random } from 'lodash-es';
 
 const cardsImgs = [
     'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
@@ -21,17 +21,18 @@ const inDeckPosition = (i: number) => ({
     x: cardXCenter,
     y: window.innerHeight - (cardHeight) + (i * -4),
     scale: 1,
-    rot: -10 + Math.random() * 20,
-    delay: i * 100,
+    angle: 30,
+    flip: 0,
+    rot: random(-10, 10, true),
 })
 
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r: number, s: number) =>
-    `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
+const trans = (r: number, a: number, f: number, s: number) =>
+    `perspective(1500px) rotateX(${a}deg) rotateY(${f || r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
 type Card = {
     value: number;
-    state: "inDeck" | "highlighted" | "inPlay" | "discarded";
+    state: "inDeck" | "highlighted-flipped" | "highlighted" | "inPlay" | "discarded";
     backgroundUrl: string;
 }
 
@@ -46,21 +47,46 @@ export function Eg() {
     } as const))))
 
     const [props, api] = useSprings(cards.current.length, i => ({
-        ...inDeckPosition(i),
-        from: { x: cardXCenter, rot: 0, scale: 1.5, y: -1000 },
-    })) // Create a bunch of springs using the helpers above
-    
-    // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-    const bind = useDrag(({ args: [index], down, movement: [mx, my], direction: [xDir, yDir], velocity }) => {
-        const Xtrigger = velocity[0] > 0.2 // If you flick hard enough it should trigger the card to fly out
-        const Ytrigger = velocity[1] > 0.2
-        
+        to: inDeckPosition(i),
+        from: { x: cardXCenter, angle: 30, rot: 0, scale: 1.5, y: -1000, flip: 0 },
+        delay: i * 100,
+    }))
+
+    const bind = useDrag(({ args: [index], down, movement: [mx, my], direction: [xDir, yDir], velocity: [xVel, yVel] }) => {
+
         if (!down) {
-            if (Xtrigger) {
-                // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-                cards.current[index].state = "discarded"
-            } else if (Ytrigger && yDir < 0) {
-                cards.current[index].state = "highlighted"
+            if (cards.current[index].state === "inDeck") {
+                const triggerHighlight = yVel > 0.2 && yDir < 0;
+                const triggerDiscard = xVel > 0.2;
+                if (triggerHighlight) {
+                    cards.current[index].state = "highlighted"
+                    setHighlightedCard(index)
+                } else if (triggerDiscard) {
+                    // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+                    cards.current[index].state = "discarded"
+                }
+            } else if (cards.current[index].state === "highlighted") {
+                const triggerFlip = xVel > 0.2;
+                const triggerDeck = yVel > 0.2 && yDir > 0;
+                if (triggerFlip) {
+                    cards.current[index].state = "highlighted-flipped"
+                } else if (triggerDeck) {
+                    setHighlightedCard(undefined)
+                    cards.current[index].state = "inDeck"
+                }
+            } else if (cards.current[index].state === "highlighted-flipped") {
+                const triggerPlay = yVel > 0.2 && yDir < 0;
+                const triggerDeck = yVel > 0.2 && yDir > 0;
+                const triggerFlip = xVel > 0.2;
+                if (triggerPlay) {
+                    cards.current[index].state = "inPlay"
+                    setHighlightedCard(undefined)
+                } else if (triggerFlip) {
+                    cards.current[index].state = "highlighted"
+                } else if (triggerDeck) {
+                    setHighlightedCard(undefined)
+                    cards.current[index].state = "inDeck"
+                }
             }
         }
 
@@ -71,32 +97,44 @@ export function Eg() {
             }
 
             const cardState = cards.current[index].state
-
             if (cardState === "discarded") {
                 return {
-                    x: (50 + cardWidth + window.innerWidth) * discardDir,
-                    rot: mx / 100 + (discardDir * 10 * velocity[0]),
-                    scale: down ? 1.1 : 1,
-                    config: { friction: 50, tension: down ? 800 : 200 },
+                    to: {
+                        x: (50 + cardWidth + window.innerWidth) * discardDir,
+                        rot: mx / 100 + (discardDir * 10 * xVel),
+                        scale: down ? 1.1 : 1,
+                        config: { friction: 50, tension: down ? 800 : 200 },
+                    }
                 }
             } else if (cardState === "highlighted") {
                 const startPos = inDeckPosition(i)
                 const scaleSize = 1.5
                 return {
-                    x: startPos.x,
-                    y: startPos.y - (cardHeight * scaleSize) / 2,
-                    scale: scaleSize,
-                    rot: 0,
+                    to: {
+                        x: startPos.x,
+                        y: startPos.y - (cardHeight * scaleSize) / 2,
+                        scale: scaleSize,
+                        rot: 0,
+                        angle: 0,
+                        flip: 0,
+                    }
+                }
+            } else if (cardState === "highlighted-flipped") {
+                return {
+                    flip: 180,
                 }
             } else {
-                // return to deck
                 const startPos = inDeckPosition(i)
                 return {
-                    x: down ? startPos.x + mx : startPos.x,
-                    y: down ? startPos.y + my - 20 : startPos.y,
-                    rot: mx / 100,
-                    scale: down ? 1.1 : startPos.scale,
-                    config: { friction: 50, tension: down ? 800 : 500 },
+                    to: {
+                        x: down ? startPos.x + mx : startPos.x,
+                        y: down ? startPos.y + my - 20 : startPos.y,
+                        rot: mx / 100,
+                        scale: down ? 1.1 : startPos.scale,
+                        config: { friction: 50, tension: down ? 800 : 500 },
+                        angle: 30,
+                        flip: 0,
+                    }
                 }
             }
         })
@@ -109,17 +147,17 @@ export function Eg() {
             }, 600)
         }
     })
-    
+
     return (
         <div className={styles.canvas}>
-            {props.map(({ x, y, rot, scale }, i) => (
+            {props.map(({ x, y, rot, scale, angle, flip, }, i) => (
                 <animated.div className={styles["card-container"]} key={i} style={{ x, y }}>
                     {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
                     <TarotCard
                         as={animated.div}
                         {...bind(i)}
                         style={{
-                            transform: interpolate([rot, scale], trans),
+                            transform: interpolate([rot, angle, flip, scale], trans),
                         }}
                         backgroundUrl={cards.current[i].backgroundUrl}
                     />
